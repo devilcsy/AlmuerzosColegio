e4d584658c77fa22aaeee7610e74543f0ec36966
Andrew17-Mora <adrojas53@ucatolica.edu.co>
2025-11-26 01:10:19 +0000

    arreglo vite

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,21 @@
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000
+  },
+ 
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+
+  preview: {
+    port: 3000,
+    host: true
   }
 })
