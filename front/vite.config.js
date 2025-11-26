99655e1ee42caac81bc3a6d7639ab4cd2db71f68
Andrew17-Mora <adrojas53@ucatolica.edu.co>
2025-11-26 01:12:52 +0000

    arreglo vite2

--- front/vite.config.js
+++ front/vite.config.js
@@ -7,15 +7,12 @@ export default defineConfig({
   server: {
     port: 3000
   },
- 
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
   },
-
   preview: {
-    port: 3000,
-    host: true
+    port: 3000
   }
 })
